a5a92e90fc7981aed6c75d7287b34418141b2669
Andron1976 <and.ron@ukr.net>
2022-05-27 00:58:53 +0300

    did task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// make p.p. 1,2
+
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+function newItem() {
+  return galleryItems.map(
+      (pics) =>
+        `<a class="gallery__item" href="${pics.original}">
+        <img class="gallery__image" src="${pics.preview}" alt="${pics.description}" />
+        </a>`
+    ).join("");
+}
+galleryEl.insertAdjacentHTML("afterbegin", newItem(galleryItems));
+
+// make 3,4
+let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
